2d29e373d045a7f8d9cc68ac40016cb69d499526
smjh950315 <103402409+smjh950315@users.noreply.github.com>
2024-06-08 19:39:21 +0800

    .

--- cyh/os/shmem_mgr.cpp
+++ cyh/os/shmem_mgr.cpp
@@ -3,35 +3,132 @@
 #include "res_mon.hpp"
 
 namespace cyh::os {
+	struct _MemBlock {
+		nuint m_size{};
+		void* m_data{};
+	};
+	// Write the size of block to it's first address
 	static void write_size_header(void* head_of_block, nuint size) {
 		if (!head_of_block) { return; }
 		nuint* pSize = (nuint*)head_of_block;
 		*pSize = size;
 	}
+	// Read the size stored in the first address of block
 	static nuint read_size_header(void* head_of_block) {
 		if (!head_of_block) { return 0; }
 		nuint* pSize = (nuint*)head_of_block;
 		return *pSize;
 	}
+	// For windows, this only work if the input handle is the last handle link to the shared memory in global
+	static void try_stop_sharing(void* handle, const char* name) {
+#ifdef __WINDOWS_PLATFORM__
+		CloseHandle(handle);
+#else
+		shm_unlink(name);
+#endif
+	}
+	// Unmap the address of current process which is mapped to the shared memory
+	static void unmap_address(void* head_of_block, nuint size) {
+		if (head_of_block) {
+#ifdef __WINDOWS_PLATFORM__
+			UnmapViewOfFile(head_of_block);
+#else
+			munmap(head_of_block, size);
+#endif
+		}
+	}
+	// Indicate wether the input handle is valid
+	static bool is_valid_shm_handle(void* handle) {
+		return
+#ifdef __WINDOWS_PLATFORM__
+			handle != nullptr;
+#else
+			(int)handle > 0;
+#endif
+	}
+	// Create a shared memory in host os and create a handle link to the shared memory
+	static void* create_shared_memory(const char* name, nuint alloc_size) {
+		return
+#ifdef __WINDOWS_PLATFORM__
+			CreateFileMappingA(
+				INVALID_HANDLE_VALUE,
+				NULL,
+				PAGE_READWRITE,
+				0,
+				static_cast<DWORD>(alloc_size),
+				name
+			);
+#else
+			(void*)shm_open(name, O_CREAT | O_RDWR, 0666);
+#endif
+	}
+	// Create a handle to an exists shared memory
+	static void* get_exist_shm_handle(const char* name, uint accessFlag) {
+		return
+#ifdef __WINDOWS_PLATFORM__
+			OpenFileMapping(accessFlag, FALSE, name);
+#else
+			shm_open(name, O_RDWR, 0666);
+#endif
+	}
+	// For windows, this will stop sharing memory if this is the last handle bound to the shared memory
+	static void close_shm_handle(void* handle) {
+#ifdef __WINDOWS_PLATFORM__
+		CloseHandle(handle);
+#else
+		close((int)handle);
+#endif
+	}
+	// This function will mark the alloc_size at the starting address of shared memory if the input handle existing a bound shared memory
+	static void* init_and_get_shm_address(void* handle, nuint alloc_size) {
+		void* ptr;
+#ifdef __WINDOWS_PLATFORM__
+		ptr = MapViewOfFile(handle, FILE_MAP_ALL_ACCESS, 0, 0, alloc_size);
+#else
+		ftruncate((int)handle, alloc_size);
+		ptr = mmap(0, alloc_size, PROT_READ | PROT_WRITE, MAP_SHARED, (int)handle, 0);
+#endif
+		if (ptr) {
+			write_size_header(ptr, alloc_size);
+		}
+		return ptr;
+	}
+	// Map the shared memory to the address in current process and get the pointer
+	static void* get_shm_address(void* handle, uint accessFlag, nuint size_to_mapping) {
+		void* ptr;
+#ifdef __WINDOWS_PLATFORM__
+		ptr = MapViewOfFile(handle, accessFlag, 0, 0, size_to_mapping);
+#else
+		ptr = mmap(0, size_to_mapping, accessFlag, MAP_SHARED, (int)handle, 0);
+#endif
+		return ptr;
+	}
+	// Create an object automatically managed the lifetime of shared memory
+	// The input handle and block will managed by SharedMemoryHolder after calling this function
+	static SharedMemoryManager::SharedMemoryHolder create_shm_holder(void*& handle, void* block, const char* name, bool is_owner) {
+		void* temp = handle;
+		handle = 0;
+		return SharedMemoryManager::SharedMemoryHolder(temp, block, name, is_owner);
+	}
+	// Create an useless holder
+	static SharedMemoryManager::SharedMemoryHolder create_invalid_holder() {
+		return SharedMemoryManager::SharedMemoryHolder{ 0, 0, "", false };
+	}
 
 	void SharedMemoryManager::_ReleaseHolder(SharedMemoryHolder* pholder) {
 		if (!pholder) { return; }
-
-#ifdef __WINDOWS_PLATFORM__
 		if (pholder->m_handle) {
-			CloseHandle(pholder->m_handle);
+			try_stop_sharing(pholder->m_handle, pholder->fname.c_str());
 		}
-#else
-		if ((int)pholder->m_handle > 0) {
-			shm_unlink(pholder->fname.c_str());
+		if (pholder->m_block) {
+			auto alloc_size = read_size_header(pholder->m_block);
+			unmap_address(pholder->m_block, alloc_size);
 		}
 		pholder->fname.clear();
-#endif
 		pholder->m_size = 0;
 		pholder->m_handle = 0;
 		pholder->m_block = 0;
 	}
-
 	void SharedMemoryManager::_MoveHolder(SharedMemoryHolder* pdst, SharedMemoryHolder* psrc) {
 		if (!pdst) { return; }
 		_ReleaseHolder(pdst);
@@ -49,39 +146,29 @@ namespace cyh::os {
 
 #endif
 	}
-	SharedMemoryManager::SharedMemoryHolder SharedMemoryManager::_OpenSharedMemory(uint accessFlag, const std::string& name, nuint openSz) {
-#ifdef __WINDOWS_PLATFORM__
-		auto handle = OpenFileMapping(accessFlag, FALSE, name.c_str());
-		if (handle) {
-			return SharedMemoryHolder{ handle, MapViewOfFile(handle, accessFlag, 0, 0, openSz) };
+	void* SharedMemoryManager::SharedMemoryHolder::data() const {
+		if (this->m_block) {
+			return ((_MemBlock*)this->m_block)->m_data;
 		}
-		return SharedMemoryHolder{ 0, 0 };
+		return nullptr;
+	}
+	nuint SharedMemoryManager::SharedMemoryHolder::capacity() const {
+		return this->m_size;
+	}
+	SharedMemoryManager::SharedMemoryHolder::SharedMemoryHolder(void* handle, void* block, const std::string& name, bool is_owner) : m_block(block), fname(name) {
+#ifdef __WINDOWS_PLATFORM__
+		this->m_handle = handle;
 #else
-		int fd = shm_open(name.c_str(), O_RDWR, 0666);
-		if (fd > 0) {
-			void* ptr = mmap(NULL, openSz, accessFlag, MAP_SHARED, fd, 0);
-			if (ptr) {
-				close(fd);
-				return SharedMemoryHolder{ 0, ptr, openSz, name };
+		close((int)handle);
+		if (is_owner) { this->m_handle = handle; }
+#endif
+		if (block) {
+			auto alloc_size = read_size_header(block);
+			if (alloc_size > sizeof(nuint)) {
+				this->m_size = alloc_size - sizeof(nuint);
 			}
-			close(fd);
-		}
-		return SharedMemoryHolder{ 0, 0, 0, "" };
-#endif
-	}
-#ifdef __WINDOWS_PLATFORM__
-	SharedMemoryManager::SharedMemoryHolder::SharedMemoryHolder(void* handle, void* block) : m_handle(handle), m_block(block) {
-		if (block) {
-			this->m_size = read_size_header(block);
 		}
 	}
-#else
-	SharedMemoryManager::SharedMemoryHolder::SharedMemoryHolder(void* handle, void* block, const std::string& _fname) : m_handle(handle), m_block(block), fname(_fname) {
-		if (block) {
-			this->m_size = read_size_header(block);
-		}
-	}
-#endif
 	SharedMemoryManager::SharedMemoryHolder::SharedMemoryHolder(SharedMemoryHolder&& other) noexcept {
 		SharedMemoryManager::_MoveHolder(this, &other);
 	}
@@ -101,50 +188,38 @@ namespace cyh::os {
 	uint SharedMemoryManager::ACCESS_READWRITE = PROT_READ | PROT_WRITE;
 #endif
 
-	SharedMemoryManager::SharedMemoryHolder SharedMemoryManager::InvalidHolder() {
-#ifdef __WINDOWS_PLATFORM__
-		return SharedMemoryHolder{ 0, 0 };
-#else
-		return SharedMemoryHolder{ 0, 0, "" };
-#endif
-	}
-
 	SharedMemoryManager::SharedMemoryHolder SharedMemoryManager::CreateSharedMemory(const std::string& name, nuint byteSize) {
-#ifdef __WINDOWS_PLATFORM__
+		nuint alloc_size = byteSize + sizeof(nuint);
 		auto mstmt = ResourceMonitor::GetMemoryStatus();
-		if (mstmt.Physical.avail > byteSize) {
-			auto handle = CreateFileMappingA(
-				INVALID_HANDLE_VALUE,
-				NULL,
-				PAGE_READWRITE,
-				0,
-				byteSize,
-				name.c_str()
-			);
-			if (handle) {
-				return SharedMemoryHolder{ handle, MapViewOfFile(handle, FILE_MAP_ALL_ACCESS, 0, 0, 0) };
+		if (mstmt.Physical.avail > alloc_size) {
+			auto handle = create_shared_memory(name.c_str(), alloc_size);
+			if (is_valid_shm_handle(handle)) {
+				auto addr = init_and_get_shm_address(handle, alloc_size);
+				if (addr) {
+					return create_shm_holder(handle, addr, name.c_str(), true);
+				}
+				close_shm_handle(handle);
 			}
 		}
-#else
-		auto fd = shm_open(name.c_str(), O_CREAT | O_RDWR, 0666);
-		if (fd > 0) {
-			ftruncate(fd, byteSize + sizeof(nuint));
-			void* ptr = mmap(0, byteSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-			if (ptr) {
-				close(fd);
-				return SharedMemoryHolder{ (void*)fd, ptr, name };
-			}
-			close(fd);
-		}
-#endif
-		return InvalidHolder();
+		return create_invalid_holder();
 	}
-
 	SharedMemoryManager::SharedMemoryHolder SharedMemoryManager::OpenSharedMemory(uint accessFlag, const std::string& name) {
-		auto tempHolder = _OpenSharedMemory(accessFlag, name, sizeof(nuint));
-		if (tempHolder.m_size) {
-			return _OpenSharedMemory(accessFlag, name, tempHolder.m_size + sizeof(nuint));
+		auto handle = get_exist_shm_handle(name.c_str(), accessFlag);
+		if (is_valid_shm_handle(handle)) {
+			auto ptr = get_shm_address(handle, SharedMemoryManager::ACCESS_READONLY, sizeof(nuint));
+			if (ptr) {
+				auto alloc_size = *((nuint*)ptr);
+				unmap_address(ptr, sizeof(nuint));
+
+				if (alloc_size) {	
+					ptr = get_shm_address(handle, accessFlag, alloc_size);
+					if (ptr) {
+						return create_shm_holder(handle, ptr, name.c_str(), false);
+					}
+				}	
+			}
+			close_shm_handle(handle);
 		}
-		return InvalidHolder();
+		return create_invalid_holder();
 	}
 };
--- cyh/os/shmem_mgr.hpp
+++ cyh/os/shmem_mgr.hpp
@@ -1,55 +1,41 @@
 #pragma once
 #include "os_.hpp"
 namespace cyh::os {
+
 	class SharedMemoryManager {
 	public:
 		class SharedMemoryHolder;
 	private:
 		static void _ReleaseHolder(SharedMemoryHolder* pholder);
 		static void _MoveHolder(SharedMemoryHolder* pdst, SharedMemoryHolder* psrc);
-		static SharedMemoryHolder _OpenSharedMemory(uint accessFlag, const std::string& name, nuint openSize);
 	public:
 
+		// An object automatically managed the lifetime of shared memory
 		class SharedMemoryHolder final {
 			friend class SharedMemoryManager;
 			void* m_handle{};
 			void* m_block{};
 			nuint m_size{};
-#ifdef __WINDOWS_PLATFORM__
-#else
-			// for unix
 			std::string fname{};
-#endif
 		public:
+			// The address mapped to the shared memory
+			void* data() const;
+			// The capacity of memory in bytes
+			nuint capacity() const;
+
 			template<class T>
-			T* GetPtr() {
-				if (this->m_block) {
-					auto temp = (char*)m_block;
-					return (T*)(temp + sizeof(nuint));
-				}
-				return nullptr;
-			}
-			nuint size() const { return this->m_size; }
-#ifdef __WINDOWS_PLATFORM__
-			SharedMemoryHolder(void* handle, void* block);
-#else
-			SharedMemoryHolder(void* handle, void* block, const std::string& _fname);
-#endif
+			T* get() const { return (T*)(this->data()); }
+
+			SharedMemoryHolder(void* handle, void* block, const std::string& name, bool is_owner);
 			SharedMemoryHolder(const SharedMemoryHolder&) = delete;
 			SharedMemoryHolder& operator=(const SharedMemoryHolder&) = delete;
 			SharedMemoryHolder(SharedMemoryHolder&& other) noexcept;
 			SharedMemoryHolder& operator=(SharedMemoryHolder&& other) noexcept;
 			~SharedMemoryHolder();
 		};
-
 		static uint ACCESS_READONLY;
-		static uint ACCESS_READWRITE;
-
-		static SharedMemoryHolder InvalidHolder();
-
+		static uint ACCESS_READWRITE;		
 		static SharedMemoryHolder CreateSharedMemory(const std::string& name, nuint byteSize);
-
 		static SharedMemoryHolder OpenSharedMemory(uint accessFlag, const std::string& name);
-
 	};
 };
